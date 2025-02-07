498e4353885052f15dd620dfdf4f6f4a25c4b489
mobile976 <151300281+mobile976@users.noreply.github.com>
2025-02-07 17:23:18 +0800

    edit nuxt-config recipe

---
+++ _nuxt/static/1738920185/results/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frecipe\u002F_nuxt\u002Fstatic\u002F1738920185",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresults",config:{_app:{basePath:"\u002Frecipe\u002F",assetsPath:"\u002Frecipe\u002F_nuxt\u002F",cdnURL:a}}}}(null));
