4b3a5beb47441e23b4d748537a62c4929ea24a3b
mobile976 <151300281+mobile976@users.noreply.github.com>
2025-02-07 18:20:36 +0800

    edit firebase

--- _nuxt/static/1738922063/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frecipe\u002F_nuxt\u002Fstatic\u002F1738922063",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Frecipe\u002F",assetsPath:"\u002Frecipe\u002F_nuxt\u002F",cdnURL:a}}}}(null));
