ce6fd06cd99a028101165133fde4b568549d4ae9
mobile976 <151300281+mobile976@users.noreply.github.com>
2025-02-07 16:32:24 +0800

    edit nuxt-config

---
+++ dist/_nuxt/static/1738917116/results/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnasha-nuxt\u002F_nuxt\u002Fstatic\u002F1738917116",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresults",config:{_app:{basePath:"\u002Fnasha-nuxt\u002F",assetsPath:"\u002Fnasha-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
